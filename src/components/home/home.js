bdae92c2c3cba105e5a24f8f44acf5f49cf66431
shashank <shashankskb18700@gmail.com>
2024-06-22 02:03:54 +0530

    add home components

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -3,14 +3,41 @@ import Sidebar from "../sidebar/sidebar";
 import Header from "../header/header";
 import Document from "../features/document/document";
 import "./home.css";
+import { ReactComponent as TaskSvg } from "../../assets/svg/homeAddTask.svg";
+import { ReactComponent as CalenderSvg } from "../../assets/svg/homeCalender.svg";
+import { ReactComponent as DocumentSvg } from "../../assets/svg/homeAddDocument.svg";
+import { ReactComponent as DrawSvg } from "../../assets/svg/homeDrawing.svg";
 
 const Home = () => {
   return (
     <div className="home">
-      {/* <Header></Header>
-      <Sidebar></Sidebar> */}
+      <Header></Header>
+
+      <div className="homeComponent">
+        <Sidebar></Sidebar>
+        <div className="homeFeature">
+          <div className="homeFeatureComponent">
+            <TaskSvg></TaskSvg>
+            <b>CREATE TASK</b>
+          </div>
+          <div className="homeFeatureComponent">
+            <CalenderSvg></CalenderSvg>
+            <b>CALENDER</b>
+          </div>
+
+          <div className="homeFeatureComponent">
+            <DocumentSvg></DocumentSvg>
+            <b>DOCUMENT</b>
+          </div>
+          <div className="homeFeatureComponent">
+            <DrawSvg></DrawSvg>
+            <b>DRAW</b>
+          </div>
+        </div>
+      </div>
+
       {/* <TaskAndProject></TaskAndProject> */}
-      <Document></Document>
+      {/* <Document></Document> */}
     </div>
   );
 };
--- src/components/routes/routes.js
+++ src/components/routes/routes.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Auth from "../authentication/auth";
 import Home from "../home/home";
 import Welcome from "../welcomePage/welcomePage";
+import Task from "../features/taskAndProject/taskAndProject";
 
 const AppRouter = () => {
   return (
@@ -9,6 +10,7 @@ const AppRouter = () => {
       <Route path="/auth" element={<Auth></Auth>}></Route>
       <Route path="/home" element={<Home></Home>}></Route>
       <Route path="/" element={<Welcome></Welcome>}></Route>
+      <Route path="/task/:id" element={<Task></Task>}></Route>
     </Routes>
   );
 };
